f44654b460805e7b0c18bcfa40c13a3eff9b62e4
Luis Santos <luis.santos@nfsconsultoria.com.br>
2021-12-16 12:50:17 -0300

    Remoção da senha

--- config/settings.js
+++ config/settings.js
@@ -5,14 +5,14 @@ const config = () => {
     case "development":
       return {
         dbpath:
-          "mongodb+srv://rm342213:Passw0rd@cluster0.2kqkl.mongodb.net/user_fiap_dev_db?retryWrites=true&w=majority",
+          "mongodb+srv://<seu usuario>:<sua senha>@cluster0.2kqkl.mongodb.net/user_fiap_dev_db?retryWrites=true&w=majority",
         jwt_key: "Navigation",
         jwt_expires_at: "5m",
       };
     case "production":
       return {
         dbpath:
-          "mongodb+srv://rm342213:Passw0rd@cluster0.2kqkl.mongodb.net/user_fiap_db?retryWrites=true&w=majority",
+          "mongodb+srv://<seu usuario>:<sua senha>@cluster0.2kqkl.mongodb.net/user_fiap_db?retryWrites=true&w=majority",
         jwt_key: "Navigation",
         jwt_expires_at: "1d",
       };
